8e478ed68e6e62d6161f015de0703e344e9ecfc0
Kevin Santos <kevinst2610@gmail.com>
2023-05-10 02:51:03 -0300

    .

---
+++ Constructor/Aluno/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Aluno{
+	
+	public:
+		
+	string nome;
+	int matricula;
+	int telefone;
+	
+	Aluno(string nome, int matricula, int telefone){
+		this->nome=nome;
+		this->matricula=matricula;
+		this->telefone=telefone;
+	}
+	
+	Aluno(){
+		nome="vazio";
+		matricula=0;
+		telefone=0;
+		
+	}
+	
+	void imprimirDados(){
+		cout<<this->nome<<endl;
+		cout<<this->matricula<<endl;
+		cout<<this->telefone<<endl;
+	}
+		
+};
+
+int main(int argc, char** argv) {
+	string x;
+	int y;
+	int z;
+	cin>>x>>y>>z;
+	Aluno alunoUm(x, y, z);
+	alunoUm.imprimirDados();
+	Aluno alunoDois;
+	alunoDois.imprimirDados();
+	
+	return 0;
+}
